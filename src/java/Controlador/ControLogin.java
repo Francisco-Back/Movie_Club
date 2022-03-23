e9381e614e52e4f76e4e4ab3f484ef6bc4d61397
Francisco-Back <fbacks@miumg.edu.gt>
2022-03-23 10:29:29 -0600

    lista Enlazada

--- src/java/Controlador/ControLogin.java
+++ src/java/Controlador/ControLogin.java
@@ -3,23 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 package Controlador;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+/*
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+*/
 
 
-import java.io.IOException;
-import java.io.PrintWriter;
-/*import javax.servlet.ServletException;
+///*
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;*/
+import javax.servlet.http.HttpServletResponse;
+//*/
 
 
 
